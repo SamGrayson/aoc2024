1cd0a0888f09687f0f1ea709f558e87a2caf3ed0
Sam Grayson <samuel.c.grayson17@gmail.com>
2024-12-01 10:29:32 -0500

    Day 1! AOC setup and ready to go

---
+++ day_01/main.py
@@ -0,0 +1,55 @@
+def getDiff(g1: list[int], g2: list[int]) -> int:
+    g1.sort()
+    g2.sort()
+
+    diff = 0
+    for idx, i in enumerate(g1):
+        diff += abs(g2[idx] - i)
+    return diff
+
+
+def getSimilarityScore(g1: list[int], g2: list[int]) -> int:
+    num_count = {}
+    for i in g2:
+        if i not in num_count:
+            num_count[i] = 1
+        else:
+            num_count[i] += 1
+
+    total_sim_score = 0
+    for i in g1:
+        total_sim_score += i * num_count.get(i, 0)
+    return total_sim_score
+
+
+def part_1():
+    with open("day_01/input.txt", "r") as file:
+        g1 = []
+        g2 = []
+        for line in file:
+            groups = line.strip().split("   ")
+            g1.append(groups[0])
+            g2.append(groups[1])
+    g1 = list(map(int, g1))
+    g2 = list(map(int, g2))
+    file.close()
+    print("Part 1: " + str(getDiff(g1, g2)))
+
+
+def part_2():
+    with open("day_01/input.txt", "r") as file:
+        g1 = []
+        g2 = []
+        for line in file:
+            groups = line.strip().split("   ")
+            g1.append(groups[0])
+            g2.append(groups[1])
+    g1 = list(map(int, g1))
+    g2 = list(map(int, g2))
+    file.close()
+    print("Part 2: " + str(getSimilarityScore(g1, g2)))
+
+
+if __name__ == "__main__":
+    part_1()
+    part_2()
---
+++ day_01/test.py
@@ -0,0 +1,22 @@
+import unittest
+from main import getDiff, getSimilarityScore
+
+
+class TestDay1(unittest.TestCase):
+    def test_distance_success(self):
+        g1 = [1, 2, 3]
+        g2 = [3, 2, 1]
+        # Should equal 0 since no diff
+        result = getDiff(g1, g2)
+        assert (result, 0)
+
+    def test_get_sim_score(self):
+        g1 = [1, 2, 3]
+        g2 = [1, 1, 1]
+        # Should equal 3, 1 appears 3 times
+        result = getSimilarityScore(g1, g2)
+        assert (result, 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
