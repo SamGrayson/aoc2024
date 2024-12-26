1f7d29303e22f7efc3bfc31d316893084d4770cf
Sam Grayson <samuel.c.grayson17@gmail.com>
2024-12-26 14:39:30 -0500

    some refactor for tracking parent man distance

--- day_21/main.py
+++ day_21/main.py
@@ -2,7 +2,7 @@ from collections import deque
 import os
 import time
 
-from utils.utils import in_bounds, string_to_grid, directions_plus
+from utils.utils import in_bounds, string_to_grid, directions_plus, manhattan_distance
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 INPUT_PATH = os.path.join(SCRIPT_DIR, "input.txt")
@@ -73,9 +73,9 @@ def recur_shortest(
     # v<<A>>^A<A>AvA<^AA>A<vAAA>^A
     for c in value:
         if rtype == "NUM":
-            paths = find_next_paths_in_grid(num_pad_local, num_pad, start, c)
+            paths = find_next_paths_in_grid_num(num_pad_local, num_pad, start, c)
         if rtype == "DIR":
-            paths = find_next_paths_in_grid(dir_pad_local, dir_pad, start, c)
+            paths = find_next_paths_in_grid_dir(dir_pad_local, dir_pad, start, c)
         for idx, p in enumerate(paths):
             paths[idx] = recur_shortest((p + "A"), max_depth, "DIR", "A", depth + 1)
         shortest += sorted(paths, key=len)[0]
@@ -84,7 +84,7 @@ def recur_shortest(
     return shortest
 
 
-def find_next_paths_in_grid(
+def find_next_paths_in_grid_num(
     test_grid_local: dict,
     test_grid: list[list[str]],
     start: str,
@@ -108,11 +108,79 @@ def find_next_paths_in_grid(
         if (cr, cc) == end:
             paths.append(path)
             short_len = len(path)
+            continue
         # Reordering due to the weight of each away from A - left buttons are farthest from "A"
         for dir in [RIGHT, UP, DOWN, LEFT]:
             nr, nc = cr + dir[0], cc + dir[1]
             if in_bounds(max_r, max_c, (nr, nc)) and test_grid[nr][nc] != "X":
-                queue.append((nr, nc, path + DIR_MAP_STR[dir]))
+                queue.append(
+                    (
+                        nr,
+                        nc,
+                        path + DIR_MAP_STR[dir],
+                    )
+                )
+    return paths
+
+
+def find_next_paths_in_grid_dir(
+    test_grid_local: dict,
+    test_grid: list[list[str]],
+    start: str,
+    target: str,
+):
+    max_r = len(test_grid)
+    max_c = len(test_grid[0])
+    start = test_grid_local[start]
+    end = test_grid_local[target]
+    queue = deque([(start[0], start[1], "", 0, (start[0], start[1]))])
+    shortest_pdist = None
+    shortest_path = None
+    visited = set()
+    paths = []
+    while queue:
+        # pdist is the distance to "A" from the next point on the grid..
+        cr, cc, path, pdist, prev_dir = queue.popleft()
+        if (cr, cc, path) in visited:
+            continue
+        visited.add((cr, cc, path))
+        # Go ahead and exit if current path is shorter than shortest path found
+        if shortest_path and len(path) > len(shortest_path):
+            continue
+        if (cr, cc) == end:
+            # If no path set yet, set stuff
+            if not shortest_path:
+                shortest_path = path
+                shortest_pdist = pdist
+                paths.append(path)
+                continue
+            # If paths are the same, but one pdist is less than another - set it
+            if len(path) == len(shortest_path) and pdist <= shortest_pdist:
+                shortest_path = path
+                shortest_pdist = pdist
+                paths.append(path)
+                continue
+            # If paths are the same, but one pdist is higher than another - skip
+            if len(path) == len(shortest_path) and pdist > shortest_pdist:
+                continue
+            continue
+        # Reordering due to the weight of each away from A - left buttons are farthest from "A"
+        for dir in [RIGHT, UP, DOWN, LEFT]:
+            nr, nc = cr + dir[0], cc + dir[1]
+            if in_bounds(max_r, max_c, (nr, nc)) and test_grid[nr][nc] != "X":
+                queue.append(
+                    (
+                        nr,
+                        nc,
+                        path + DIR_MAP_STR[dir],
+                        pdist
+                        + manhattan_distance(
+                            dir_pad_local[DIR_MAP_STR[dir]],
+                            prev_dir,
+                        ),
+                        dir_pad_local[DIR_MAP_STR[dir]],
+                    )
+                )
     return paths
 
 
@@ -127,11 +195,16 @@ def part_1():
 
 def part_2():
     with open(INPUT_PATH) as file:
-        print(file.read())
+        total = 0
+        for idx, l in enumerate(file.readlines()):
+            path = recur_shortest(l.strip(), 26)
+            total += len(path) * int(l.replace("A", ""))
+            print(str(idx + 1) + "complete: " + total)
+    print("Part 2: " + str(total))
 
 
 if __name__ == "__main__":
     start_time = time.time()
     part_1()
-    # part_2()
+    part_2()
     print("Finished in: " + str(round(time.time() - start_time)) + "s")
--- utils/utils.py
+++ utils/utils.py
@@ -17,7 +17,7 @@ directions_corners = [
 ]
 
 
-def manhattan_distance(p1, p2):
+def manhattan_distance(p1: tuple[int, int], p2: tuple[int, int]):
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
 
